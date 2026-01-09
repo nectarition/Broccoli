d5caad8924f862bebc1127ebd0474795c431c49e
SAIGUSA Tomotada <ts@nectarition.jp>
2026-01-09 12:46:21 +0900

    RSS の URL が undefined になる不具合を修正

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,20 +3,33 @@ import { getCollection } from 'astro:content'
 import rss from '@astrojs/rss'
 import config from '../config'
 
+const categoryName = {
+  posts: '雑記',
+  releases: 'お知らせ'
+}
+
 export const GET = async (context: AstroGlobal) => {
   const postCollection = await getCollection('posts');
+  const releaseCollection = await getCollection('releases');
+
   const posts = postCollection
     .sort((a, b) => b.data.date.getTime() - a.data.date.getTime())
+
+  const releases = releaseCollection
+    .sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
+
+  const items = [...posts, ...releases].sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
 
   return rss({
     title: config.siteName,
     description: config.description,
     site: context.site ?? '',
-    items: posts.map(p => ({
-      title: p.data.title,
-      pubDate: p.data.date,
-      description: p.data.description,
-      link: `/posts/${p.slug}`
+    items: items.map(i => ({
+      title: i.data.title,
+      pubDate: i.data.date,
+      description: i.data.description,
+      link: `/${i.collection}/${i.id}`,
+      categories: [categoryName[i.collection]],
     })),
     customData: `<language>ja-JP</language>`
   })
